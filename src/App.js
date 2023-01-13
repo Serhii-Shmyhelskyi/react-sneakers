d4510663ea829274b30b4be033aae4757e54eb18
Serhii Shmyhelskyi <siro181a@gmai.com>
2023-01-13 14:00:19 +0200

    add cart, cssreset

--- src/App.js
+++ src/App.js
@@ -2,6 +2,49 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+
+          <h2>Кошик <img className="cartItemRemoveBtn" src="/img/btn-remove.svg" alt="Remove" /></h2>
+
+          <div className="items">
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg">
+              </div>
+              <div className="cartItemWrapperT">
+                <p>Чоловічі кросівки Nike Air Max 270</p>
+                <b>5395 грн</b>
+              </div>
+              <img className="cartItemRemoveBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg">
+              </div>
+              <div className="cartItemWrapperT">
+                <p>Чоловічі кросівки Nike Air Max 270</p>
+                <b>5395 грн</b>
+              </div>
+              <img className="cartItemRemoveBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+          <div className="itemsWrapperPrice">
+            <ul>
+              <li>
+                <span>Всього</span>
+                <div></div>
+                <b>10790 грн</b>
+              </li>
+              <li>
+                <span>Податок 5%:</span>
+                <div></div>
+                <b>1079 грн</b>
+              </li>
+            </ul>
+            <button className="greenButton">Оформити замовлення <img src="/img/arrow.svg" alt="Arrow" /> </button>
+          </div>
+        </div>
+      </div>
+
       <header>
         <div className="headerLeft">
           <img width={40} height={40} src="/img/logo.png" alt="" />
